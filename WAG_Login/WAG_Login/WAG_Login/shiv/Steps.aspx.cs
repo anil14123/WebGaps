955db930fb9d79fde0d1c8797f90e64b58795c61
anil kumar potlapally <anil.silverlight@gmail.com>
2015-09-13 10:48:48 +0530

    commit

--- WAG_Login/WAG_Login/WAG_Login/shiv/Steps.aspx.cs
+++ WAG_Login/WAG_Login/WAG_Login/shiv/Steps.aspx.cs
@@ -11,7 +11,7 @@ namespace WAG_Login.shiv
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Redirect("designer.aspx");
+            Response.Redirect("sitesmanager.aspx");
         }
     }
 }
