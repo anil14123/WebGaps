f0b67904e1ec8c441be89d13da76c197f4d49c31
anil kumar potlapally <anil.silverlight@gmail.com>
2015-09-26 15:48:19 +0530

    COMMIT DOWNLOAD DONE

--- WAG_Login/WAG_Login/WAG_Login/shiv/TypeScript/_Classes/SaveJQ.ts
+++ WAG_Login/WAG_Login/WAG_Login/shiv/TypeScript/_Classes/SaveJQ.ts
@@ -28,7 +28,7 @@ export module Save {
             var errorHandler = new impError.ErrorHandle.ErrorJQ();
 
             if ((data.d.Error == "" || data.d.Error == null) && data.d.Success == true) {
-                errorHandler.ActionSuccess("Download Started...<br> if download is not started click on this link <a class='download-site-link' href='" + data.d.Link + "' > download </a>");
+                errorHandler.ActionSuccess("Download link : <a class='download-site-link' href='" + data.d.Link + "' > click here </a>");
 
             }
             else {
