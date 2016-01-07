e9e0ad8599abc7bc6b5319febf434760008d3383
anil kumar potlapally <anil.silverlight@gmail.com>
2016-01-07 23:10:18 +0530

    commit

--- WAG_Login/WAG_Login/WAG_Login/shiv/TypeScript/Controls/ImageJQ.ts
+++ WAG_Login/WAG_Login/WAG_Login/shiv/TypeScript/Controls/ImageJQ.ts
@@ -254,6 +254,8 @@ export module Image {
                     plusContainer.addClass("design-css");
                     plusContainer.addClass("design-empty-text-css");
                     plusContainer.removeClass("jq-plus-container");
+                    plusContainer.find(".jq-plus-element-content").addClass("jq-plus-element-content-image");
+
                     var emptycontainer = document.createElement("div");
 
                     plusContainer.find(".adjust-image-text-other").remove();
